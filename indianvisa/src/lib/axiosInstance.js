2335daea0b7226a91aed4bc8c41cde76ba040478
ShobujDash <158812287+ShobujDash@users.noreply.github.com>
2025-03-23 23:30:36 +0600

    Update axiosInstance.js

--- indianvisa/src/lib/axiosInstance.js
+++ indianvisa/src/lib/axiosInstance.js
@@ -4,8 +4,12 @@ import axios from "axios";
 const API_BASE_URL =
   process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000/api";
 
+// const axiosInstance = axios.create({
+//   baseURL: API_BASE_URL,
+//   withCredentials: true, // Cookie পাঠানোর জন্য
+// });
 const axiosInstance = axios.create({
-  baseURL: API_BASE_URL,
+  baseURL: "https://indianvisa-fullstack.onrender.com/api",
   withCredentials: true, // Cookie পাঠানোর জন্য
 });
 
